9de47428a26fb950e5347a9404596b844401dce5
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-04-13 20:33:42 +0200

    adding table attributes

--- src/main/java/com/amigosapi/dive/student/student.java
+++ src/main/java/com/amigosapi/dive/student/student.java
@@ -1,4 +1,13 @@
 package com.amigosapi.dive.student;
 
+import java.time.LocalDate;
+
 public class student {
+
+    private long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dob;
+
 }
