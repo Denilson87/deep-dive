3538dd3749de4bd6a00dfe0ae496a5b11b0e0314
Denilson Mucavele <denilsonmucavel@outlook.com>
2022-04-13 20:37:15 +0200

    adding constructors

--- src/main/java/com/amigosapi/dive/student/student.java
+++ src/main/java/com/amigosapi/dive/student/student.java
@@ -10,4 +10,28 @@ public class student {
     private Integer age;
     private LocalDate dob;
 
+    public student() {
+    }
+
+    public student(long id,
+                   String name,
+                   String email,
+                   Integer age,
+                   LocalDate dob) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dob = dob;
+    }
+
+    public student(String name,
+                   String email,
+                   Integer age,
+                   LocalDate dob) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dob = dob;
+    }
 }
